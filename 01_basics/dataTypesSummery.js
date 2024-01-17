295bf1f01dd1c114561e42751835ba6abe809480
ayushindurkar <85801084+ayushindurkar@users.noreply.github.com>
2024-01-17 05:19:14 +0000

    heep and stack added

--- 01_basics/dataTypesSummery.js
+++ 01_basics/dataTypesSummery.js
@@ -47,3 +47,26 @@ console.log(typeof anotherId);
 //Array - object
 //Function - function(object)
 //Object - object
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "hiteshchoudharydotcom"
+
+let anothername = myYoutubename
+
+anothername = "chaiaurcode"
+
+console.log(myYoutubename); console.log(anothername);
+
+let userOne = {
+
+email: "user@google.com",
+
+upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email)
